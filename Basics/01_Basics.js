2d95b22496a0f6d08592bcadf4878b850ef56aa6
@Code <60218368+raja53a@users.noreply.github.com>
2024-05-29 04:20:10 +0000

    Added Event Listener

--- Basics/01_Basics.js
+++ Basics/01_Basics.js
@@ -4,10 +4,20 @@
 
 // message.innerText = message.innerText + " from Apna College"
 
-let divs = document.querySelectorAll(".box");
+// let divs = document.querySelectorAll(".box");
 
-let indx = 0
-for(item of divs){
-    item.innerText = `New Div ${indx}`;
-    indx++
-}
+// let indx = 0
+// for(item of divs){
+//     item.innerText = `New Div ${indx}`;
+//     indx++
+// }
+
+
+let btn = document.createElement("button");
+btn.innerText = "Submit";
+
+let msg = document.querySelector("div")
+msg.after(btn)
+
+// let heading = document.querySelector("h2")
+// heading.remove();
---
+++ Basics/02_Events.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("#btn");
+let heading = document.querySelector("h2");
+
+
+
+btn.addEventListener("click", () => {
+    btn.innerText = "Subscribed";
+});
