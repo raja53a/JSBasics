ed08daf31b53056a7e8eedf13b8b0154b670eaad
@Code <60218368+raja53a@users.noreply.github.com>
2024-05-29 02:32:19 +0000

    accessing divs using JS

---
+++ Basics/01_Basics.js
@@ -0,0 +1,11 @@
+// let message = document.querySelector("h2")
+
+// console.dir(message.innerText);
+
+// message.innerText = message.innerText + " from Apna College"
+
+let divs = document.querySelectorAll(".box");
+
+divs[0].innerText = "new value 1"
+divs[1].innerText = "new value 2"
+divs[2].innerText = "new value 3"
