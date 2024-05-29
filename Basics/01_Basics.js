b51e58bcf63849096caf272df23165d172c5a4c4
@Code <60218368+raja53a@users.noreply.github.com>
2024-05-29 02:37:00 +0000

    For Loop

--- Basics/01_Basics.js
+++ Basics/01_Basics.js
@@ -6,6 +6,8 @@
 
 let divs = document.querySelectorAll(".box");
 
-divs[0].innerText = "new value 1"
-divs[1].innerText = "new value 2"
-divs[2].innerText = "new value 3"
+let indx = 0
+for(item of divs){
+    item.innerText = `New Div ${indx}`;
+    indx++
+}
